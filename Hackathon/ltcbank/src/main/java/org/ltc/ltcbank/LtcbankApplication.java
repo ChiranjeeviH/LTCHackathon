7f2dee92d515835165d438f2e7e22b36ca58df29
Chiranjeevi Haridasula <chiranjeeviharidasula@gmail.com>
2025-03-03 15:10:52 +0530

    Missed files

---
+++ Hackathon/ltcbank/src/main/java/org/ltc/ltcbank/LtcbankApplication.java
@@ -0,0 +1,13 @@
+package org.ltc.ltcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtcbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtcbankApplication.class, args);
+	}
+
+}
---
+++ Hackathon/ltcbank/src/test/java/org/ltc/ltcbank/LtcbankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ltc.ltcbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtcbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
