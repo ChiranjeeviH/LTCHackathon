145521faf234b4721f71a33ff1f7fa5747bffe44
Chiranjeevi Haridasula <chiranjeeviharidasula@gmail.com>
2025-03-04 16:12:05 +0530

    Transfer issue fix

--- Hackathon/ltcbank/src/main/java/org/ltc/ltcbank/service/TransactionService.java
+++ Hackathon/ltcbank/src/main/java/org/ltc/ltcbank/service/TransactionService.java
@@ -3,6 +3,7 @@ package org.ltc.ltcbank.service;
 import org.ltc.ltcbank.dto.TransactionDTO;
 import org.ltc.ltcbank.entity.Account;
 import org.ltc.ltcbank.entity.Transaction;
+import org.ltc.ltcbank.repository.AccountRepository;
 import org.ltc.ltcbank.repository.TransactionRepository;
 import org.ltc.ltcbank.utility.TransactionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +18,33 @@ public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     public List<Transaction> getTransactions(Account account) {
         return transactionRepository.findByAccountId(account.getId());
     }
 
     public TransactionDTO transferFunds(Account fromAccount, Account toAccount, Double amount) {
-        // Implement the logic for transferring funds
-        // For now, just returning a new Transaction object
+        if (fromAccount.getBalance() < amount) {
+            throw new RuntimeException("Insufficient funds to transfer");
+        }
+
         Transaction transaction = new Transaction();
         transaction.setFromAccountId(fromAccount.getAccountNumber());
         transaction.setToAccountId(toAccount.getAccountNumber());
         transaction.setAmount(amount);
         transaction.setTimestamp(LocalDateTime.now());
-        return TransactionUtil.getTransactionDTO(transactionRepository.save(transaction));
+
+        fromAccount.setBalance(fromAccount.getBalance() - amount);
+        toAccount.setBalance(toAccount.getBalance() + amount);
+
+        transactionRepository.save(transaction);
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+
+        return TransactionUtil.getTransactionDTO(transaction);
     }
 
     public List<TransactionDTO> getAllTransactions() {
